951ee761d314186bf4b9d46899435f090099ce2f
Qassimemile <amil_lee@hotmail.com>
2025-08-02 11:37:34 +0300

    تحديث: تعديل بعض الملفات

--- next.config.mjs
+++ next.config.mjs
@@ -7,9 +7,6 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
   },
-  experimental: {
-    optimizeCss: true,
-  },
   compress: true,
   poweredByHeader: false,
   reactStrictMode: true,
